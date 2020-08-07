366a0da82b3d34b1978fa5b7e576acbf949e28dc
cbonoz <chrisdistrict@gmail.com>
2020-08-07 17:22:05 -0400

    styling

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {File, FilePond} from "react-filepond";
-import {ListGroup, ListGroupItem,} from "react-bootstrap";
-import api, {getErrorMessage} from "../helpers/api";
-import {toast} from "react-toastify";
+import { File, FilePond } from "react-filepond";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import api, { getErrorMessage } from "../helpers/api";
+import { toast } from "react-toastify";
 import createReactClass from "create-react-class";
 
 const FileUploader = createReactClass({
@@ -10,7 +10,7 @@ const FileUploader = createReactClass({
     this.setState({
       files: [],
       errorText: null,
-    })
+    });
   },
 
   showToast(message) {
@@ -18,16 +18,8 @@ const FileUploader = createReactClass({
   },
 
   // handle file upload. https://pqina.nl/filepond/docs/patterns/api/server/
-  handleProcessing(
-    fieldName,
-    file,
-    metadata,
-    load,
-    error,
-    progress,
-    abort
-  ) {
-    this.setState({errorText: ""});
+  handleProcessing(fieldName, file, metadata, load, error, progress, abort) {
+    this.setState({ errorText: "" });
     // console.log(JSON.stringify(load))
     const reader = new FileReader();
     reader.onload = () => {
@@ -46,12 +38,12 @@ const FileUploader = createReactClass({
           this.showToast(`File ${fileName} uploaded!`);
           load("done");
           if (this.props.successCallback) {
-            this.props.successCallback(res.data)
+            this.props.successCallback(res.data);
           }
         })
         .catch((err) => {
           const msg = `Error uploading file: ${getErrorMessage(err)}`;
-          this.setState({errorText: msg});
+          this.setState({ errorText: msg });
           error(msg);
         });
 
@@ -59,34 +51,38 @@ const FileUploader = createReactClass({
       //   self.setState({ showModal: true, fileContent, sizeKb });
       // console.log(file.name, fileAsBinaryString)
     };
-    reader.onabort = () => this.setState({errorText: "File reading aborted"});
-    reader.onerror = () => this.setState({errorText: "File reading failed"});
+    reader.onabort = () => this.setState({ errorText: "File reading aborted" });
+    reader.onerror = () => this.setState({ errorText: "File reading failed" });
 
     reader.readAsBinaryString(file);
   },
 
   render() {
-    const { files, errorText } = this.state
+    const { files, errorText } = this.state;
     return (
-        <div className="file-uploader">
-          <ListGroup>
-            <ListGroupItem bsStyle="info">Upload document</ListGroupItem>
-            <ListGroupItem>
-              <FilePond
-                  allowMultiple={false}
-                  maxFiles={1}
-                  server={{ process: this.handleProcessing }}
-                  oninit={() => console.log("init")}
-              >
-                {files.map((file) => (
-                    <File key={file} source={file} />
-                ))}
-              </FilePond>
-              {errorText && <p className="error-text">{errorText}</p>}
-            </ListGroupItem>
-          </ListGroup>
-        </div>
-    )
+      <div className="file-uploader">
+        <ListGroup>
+          <ListGroupItem bsStyle="info">
+            {this.props.compare
+              ? `Compare existing document`
+              : `Upload new document`}
+          </ListGroupItem>
+          <ListGroupItem>
+            <FilePond
+              allowMultiple={false}
+              maxFiles={1}
+              server={{ process: this.handleProcessing }}
+              oninit={() => console.log("init")}
+            >
+              {files.map((file) => (
+                <File key={file} source={file} />
+              ))}
+            </FilePond>
+            {errorText && <p className="error-text">{errorText}</p>}
+          </ListGroupItem>
+        </ListGroup>
+      </div>
+    );
   },
 });
 
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import FileChain from "./FileChain";
 import FileUploader from "./FileUploader";
 import FileInfo from "./FileInfo";
-import {Button, Col, Row} from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 import api from "../helpers/api";
-import {useAuth} from "../auth";
-import {toast} from "react-toastify";
+import { useAuth } from "../auth";
+import { toast } from "react-toastify";
+import Loader from "react-loader-spinner";
 
 const MAX_BLOCKS = 15;
 
@@ -57,10 +58,14 @@ const Upload = () => {
     return null;
   }
 
-  if (!!selectedFile) {
+  // If we have a selected file show it.
+  const hasSelectedFile = !!selectedFile;
+  if (hasSelectedFile) {
     return (
-      <div>
-        <Button onClick={() => setSelectedFile(null)}>Go back</Button>
+      <div className="upload-page">
+        <div className="header-row">
+          <Button onClick={() => setSelectedFile(null)}>Go back</Button>
+        </div>
         <FileInfo selectedFile={selectedFile} />
       </div>
     );
@@ -70,18 +75,29 @@ const Upload = () => {
     <div className="upload-page">
       <div className="header-row">Your Documents</div>
 
-      <Row className="show-grid">
-        <Col xs={12} md={5}>
-          <FileUploader addBlocks={(blocks) => addBlocks(blocks)} compare={false} />
-        </Col>
-        <Col xs={12} md={7}>
-          <FileChain
-            loading={loading}
-            blockFiles={blockFiles}
-            setSelectedFile={setSelectedFile}
-          />
-        </Col>
-      </Row>
+      {loading && (
+        <div className="centered loading-margin">
+          <Loader type="ThreeDots" color="#007bff" height="50" width="50" />
+        </div>
+      )}
+
+      {!loading && (
+        <Row className="show-grid">
+          <Col xs={12} md={5}>
+            <FileUploader
+              addBlocks={(blocks) => addBlocks(blocks)}
+              compare={false}
+            />
+          </Col>
+          <Col xs={12} md={7}>
+            <FileChain
+              loading={loading}
+              blockFiles={blockFiles}
+              setSelectedFile={setSelectedFile}
+            />
+          </Col>
+        </Row>
+      )}
     </div>
   );
 };
