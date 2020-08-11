987cd01743956f76c4678a5713b3e3c3a09ad4bd
cbonoz <chrisdistrict@gmail.com>
2020-08-11 19:58:19 -0400

    err

--- src/helpers/DateUtil.js
+++ src/helpers/DateUtil.js
@@ -1,22 +1,25 @@
 const library = (function () {
+  function formatEpochSeconds(epochSeconds) {
+    return formatDate(epochSecondsToDate(epochSeconds));
+  }
 
-    function formatEpochSeconds(epochSeconds) {
-        return formatDate(epochSecondsToDate(epochSeconds));
+  function epochSecondsToDate(epochSeconds) {
+    const d = new Date(epochSeconds * 1000);
+    if (d.getFullYear() === 1969) {
+      console.log("e", epochSeconds, d);
     }
+    return d;
+  }
 
-    function epochSecondsToDate(epochSeconds) {
-        return new Date(epochSeconds * 1000);
-    }
+  function formatDate(date) {
+    return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+  }
 
-    function formatDate(date) {
-        return date.toLocaleDateString() + " " + date.toLocaleTimeString();
-    }
-
-    return {
-        formatEpochSeconds,
-        epochSecondsToDate,
-        formatDate
-    };
+  return {
+    formatEpochSeconds,
+    epochSecondsToDate,
+    formatDate,
+  };
 })();
 
 module.exports = library;
